3d778394409889c91cc0ffcbd5fbaabb59801d30
James Pier <jpier2012@gmail.com>
2019-04-08 18:44:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -23,7 +23,7 @@ def first_challenge
   }
 
   #remember to return your newly altered contacts hash!
-  puts contacts["Freddy Mercury"][:favorite_icecream_flavors]
+  puts contacts["Freddy Mercury"]
   #contacts
 end
 
